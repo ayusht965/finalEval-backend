72d0b9ad4e9f1d5d99b2c5aa1f7b5acdab5d8d93
Ayush Tyagi <ak7046243581@gmail.com>
2023-03-10 08:55:42 +0530

    chore: refactor code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
+const cors = require('cors');
+const contentRouter = require('./routes/index.js');
+const port = 5000;
+app.use(cors());
+app.use(express.json());
+app.use('/', contentRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
